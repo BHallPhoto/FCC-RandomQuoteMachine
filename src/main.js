fad57f4a91a7b539a32038bb6d345ba62c21a649
Bristol Hall <bristolhall@gmail.com>
2018-04-16 19:05:01 -0700

    save

---
+++ src/main.js
@@ -0,0 +1,35 @@
+// eslint-disable-next-line
+$(document).ready(function() {
+    var dataUrl = "http://quotes.stormconsultancy.co.uk/random.json";
+    function getJson(data) {
+        $.ajax({
+           dataType: "json",
+           url: dataUrl,
+           success: function(data) {
+              parseData(data);
+           }
+        });
+    }
+
+    function parseData(data) {
+        console.log(data);
+        $(".author").html(data.author);
+        $(".quotedText").html(data.quote);
+        $(".quoteLink").attr("href", data.permalink);
+    }
+
+    $("#new-quote").on("click", function() {
+        getJson();
+    });
+
+    $("#tweet-quote").on("click", function() {
+        var author = document.querySelector(".author").innerText,
+            quote = document.querySelector(".quotedText").innerText
+
+        window.open("https://www.twitter.com/intent/tweet?text=" + " \"" + quote + "\"  ~ " + author);
+    })
+
+    window.onload = getJson(dataUrl);
+    
+
+})
