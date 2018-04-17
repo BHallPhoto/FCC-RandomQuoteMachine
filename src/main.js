4b147182c0bf2bf24c1ab13e58218146c1036b57
Bristol Hall <bristolhall@gmail.com>
2018-04-17 16:25:47 -0700

    save

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 // eslint-disable-next-line
 $(document).ready(function() {
+
     var dataUrl = "https://cors-anywhere.herokuapp.com/http://quotes.stormconsultancy.co.uk/random.json";
+
+    // Get JSON data for ID, Author, Quote, and Permalink.
     function getJson(data) {
         $.ajax({
            dataType: "json",
@@ -11,12 +14,14 @@ $(document).ready(function() {
         });
     }
 
+    // Parse the data to the correct tags
     function parseData(data) {
         $(".author").html(data.author);
         $(".quotedText").html(data.quote);
         $(".quoteLink").attr("href", data.permalink);
     }
 
+    // Used to change color, will work on to fade in the color.
     function colorFade() {
         var color = [];
 
@@ -30,19 +35,20 @@ $(document).ready(function() {
         });
     }
 
+    // Click function to activate new Quote and change color
     $("#new-quote").on("click", function() {
         getJson();
         colorFade();
     });
 
+    // Click function to tweet the quote
     $("#tweet-quote").on("click", function() {
         var author = document.querySelector(".author").innerText,
             quote = document.querySelector(".quotedText").innerText
         
         window.open("https://www.twitter.com/intent/tweet?text=" + " \"" + quote + "\"  ~ " + author);
-    })
+    });
 
     window.onload = getJson(dataUrl);
-    
 
 })
